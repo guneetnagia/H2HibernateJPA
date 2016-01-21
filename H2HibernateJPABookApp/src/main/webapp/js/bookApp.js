3e1a719a3f177d895c2be5db549b28c5070d3e3d
guneet <guneetnagia24@gmail.com>
2016-01-21 20:45:30 +0530

    UI day2

---
+++ H2HibernateJPABookApp/src/main/webapp/js/bookApp.js
@@ -0,0 +1,34 @@
+(function(){
+	"use strict";
+	var myApp = angular.module("bookApp",["ui.router"]); 
+	myApp.config(["$stateProvider","$urlRouterProvider",
+	              	function($stateProvider,$urlRouterProvider){
+		
+		$urlRouterProvider.otherwise("/");
+		
+		$stateProvider
+			.state('home',{
+				url:"/",
+				templateUrl:"../views/welcomeView.html",
+			})
+			.state('addBook',{
+				url:"/addBook",
+				templateUrl:"../views/addBook.html",
+				//controller:""
+			})
+			.state('editBook',{
+				url:"/editBook",
+				templateUrl:"../views/editBook.html",
+				//controller:""
+			})
+			.state('searchBook',{
+				url:"/searchBook",
+				templateUrl:"../views/findBook.html",
+				//controller:""
+			})
+			.state('about',{
+				url:"/about",
+				templateUrl:"../views/about.html",
+			})
+	}]);
+}());
